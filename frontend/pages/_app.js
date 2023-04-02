14bfa83dd75ba4703386b94f2dddd70d45a9cef6
RazvanDRL <razdir97@gmail.com>
2023-04-02 17:21:44 +0300

    dsadsasdas

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,14 +1,5 @@
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider } from '@supabase/auth-helpers-react'
-import { useState } from 'react'
+import '@/styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  const [supabase] = useState(() => createBrowserSupabaseClient())
-
-  return (
-    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
-      <Component {...pageProps} />
-    </SessionContextProvider>
-  )
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
-export default MyApp
---
+++ frontend/pages/api/hello.js
@@ -0,0 +1,10 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({ name: 'John Doe' })
+}
+
+// search in the database for someone with the email razdir97@gmail.com using supabase
+
+// do the code below
+
