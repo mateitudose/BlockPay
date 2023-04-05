66467bc03adb92a943dfcfdd80de95d6f7883bc6
RazvanDRL <razdir97@gmail.com>
2023-04-05 13:21:40 +0300

    sadasdas

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -18,7 +18,7 @@ export default function Login() {
 
                     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
 
-                        <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+                        <div className="bg-white px-4 py-8 shadow rounded-xl sm:px-10">
                             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
                             <p className="mt-2 text-center text-sm text-gray-600">
                                 Or{' '}
