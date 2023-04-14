892f0afb40e94719be916d85d0f1f4c7d68b72fa
RazvanDRL <razdir97@gmail.com>
2023-04-14 17:11:27 +0300

    dfghj

--- frontend/pages/[id].js
+++ frontend/pages/[id].js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { supabase } from '@/lib/supabaseClient';
-
 const fetch = require('node-fetch');
 
 const apiUrl = 'https://api.coingecko.com/api/v3/simple/price?ids=binancecoin&vs_currencies=usd';
 
 
 // Page component
-const Transaction = ({ transaction }) => {
+const Checkout = ({ checkout }) => {
     function usdToBNB(usdAmount) {
         return fetch(apiUrl)
             .then(response => response.json())
@@ -18,24 +17,235 @@ const Transaction = ({ transaction }) => {
             })
             .catch(error => console.error(error));
     }
-    usdToBNB(transaction.price_in_usd)
-        .then(bnbAmount => console.log(`$${transaction.price_in_usd} USD is equivalent to ` + bnbAmount + ' BNB'))
+    usdToBNB(checkout.price_in_usd)
+        .then(bnbAmount => console.log(`$${checkout.price_in_usd} USD is equivalent to ` + bnbAmount + ' BNB'))
         .catch(error => console.error(error));
     return (
-        <div>
-            <title>Transaction Details</title>
-            <h1>Transaction Details</h1>
-            {/* Render the transaction details */}
-            <div>
-                <p>ID: {transaction.id}</p>
-                <p>Description: {transaction.price_in_usd}</p>
-                {/* Add more fields as needed */}
+        <div className="bg-white">
+            {/* Background color split screen for large screens */}
+            <div className="fixed left-0 top-0 hidden h-full w-1/2 bg-transparent lg:block" aria-hidden="true" />
+            <div className="fixed right-0 top-0 hidden h-full w-1/2 bg-white lg:block shadow-md shadow-grey"  aria-hidden="true" />
+
+            <div className="relative mx-auto grid max-w-7xl grid-cols-1 gap-x-16 lg:grid-cols-2 lg:px-8 lg:pt-16">
+                <h1 className="sr-only">Checkout</h1>
+
+                <section
+                    aria-labelledby="summary-heading"
+                    className="bg-indigo-900 py-12 text-grey-300 md:px-10 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:w-full lg:max-w-lg lg:bg-transparent lg:px-0 lg:pb-24 lg:pt-0"
+                >
+                    <div className="mx-auto max-w-2xl px-4 lg:max-w-none lg:px-0">
+                        <h2 id="summary-heading" className="sr-only">
+                            Order summary
+                        </h2>
+
+                        <dl>
+                            <dt className="text-sm font-medium">Amount due</dt>
+                            <dd className="mt-1 text-3xl font-bold tracking-tight text-black">${checkout.price_in_usd}</dd>
+                        </dl>
+
+                        <dl className="space-y-6 border-t border-white border-opacity-10 pt-6 text-sm font-medium">
+                            <div className="flex items-center justify-between">
+                                <dt>Subtotal</dt>
+                                <dd>${checkout.price_in_usd}</dd>
+                            </div>
+
+                            <div className="flex items-center justify-between">
+                                <dt>Taxes</dt>
+                                <dd>$0</dd>
+                            </div>
+
+                            <div className="flex items-center justify-between border-t border-white border-opacity-10 pt-6 text-black">
+                                <dt className="text-base">Total</dt>
+                                <dd className="text-base">$642.60</dd>
+                            </div>
+                        </dl>
+                    </div>
+                </section>
+
+                <section
+                    aria-labelledby="payment-and-shipping-heading"
+                    className="py-16 lg:col-start-2 lg:row-start-1 lg:mx-auto lg:w-full lg:max-w-lg lg:pb-24 lg:pt-0"
+                >
+                    <h2 id="payment-and-shipping-heading" className="sr-only">
+                        Payment and shipping details
+                    </h2>
+
+                    <form>
+                        <div className="mx-auto max-w-2xl px-4 lg:max-w-none lg:px-0">
+                            <div>
+                                <h3 id="contact-info-heading" className="text-lg font-medium text-gray-900">
+                                    Contact information
+                                </h3>
+
+                                <div>
+                                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                        Email
+                                    </label>
+                                    <div className="mt-2">
+                                        <input
+                                            type="email"
+                                            name="email"
+                                            id="email"
+                                            className="block w-full rounded-md border-0 py-1.5 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            placeholder="you@example.com"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="mt-10">
+                                <h3 className="text-lg font-medium text-gray-900">Payment details</h3>
+
+                                <div className="mt-6 grid grid-cols-3 gap-x-4 gap-y-6 sm:grid-cols-4">
+                                    <div className="col-span-3 sm:col-span-4">
+                                        <label htmlFor="card-number" className="block text-sm font-medium text-gray-700">
+                                            Card number
+                                        </label>
+                                        <div className="mt-1">
+                                            <input
+                                                type="text"
+                                                id="card-number"
+                                                name="card-number"
+                                                autoComplete="cc-number"
+                                                className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div className="col-span-2 sm:col-span-3">
+                                        <label htmlFor="expiration-date" className="block text-sm font-medium text-gray-700">
+                                            Expiration date (MM/YY)
+                                        </label>
+                                        <div className="mt-1">
+                                            <input
+                                                type="text"
+                                                name="expiration-date"
+                                                id="expiration-date"
+                                                autoComplete="cc-exp"
+                                                className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <label htmlFor="cvc" className="block text-sm font-medium text-gray-700">
+                                            CVC
+                                        </label>
+                                        <div className="mt-1">
+                                            <input
+                                                type="text"
+                                                name="cvc"
+                                                id="cvc"
+                                                autoComplete="csc"
+                                                className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="mt-10">
+                                <h3 className="text-lg font-medium text-gray-900">Shipping address</h3>
+
+                                <div className="mt-6 grid grid-cols-1 gap-x-4 gap-y-6 sm:grid-cols-3">
+                                    <div className="sm:col-span-3">
+                                        <label htmlFor="address" className="block text-sm font-medium text-gray-700">
+                                            Address
+                                        </label>
+                                        <div className="mt-1">
+                                            <input
+                                                type="text"
+                                                id="address"
+                                                name="address"
+                                                autoComplete="street-address"
+                                                className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <label htmlFor="city" className="block text-sm font-medium text-gray-700">
+                                            City
+                                        </label>
+                                        <div className="mt-1">
+                                            <input
+                                                type="text"
+                                                id="city"
+                                                name="city"
+                                                autoComplete="address-level2"
+                                                className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <label htmlFor="region" className="block text-sm font-medium text-gray-700">
+                                            State / Province
+                                        </label>
+                                        <div className="mt-1">
+                                            <input
+                                                type="text"
+                                                id="region"
+                                                name="region"
+                                                autoComplete="address-level1"
+                                                className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <label htmlFor="postal-code" className="block text-sm font-medium text-gray-700">
+                                            Postal code
+                                        </label>
+                                        <div className="mt-1">
+                                            <input
+                                                type="text"
+                                                id="postal-code"
+                                                name="postal-code"
+                                                autoComplete="postal-code"
+                                                className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="mt-10">
+                                <h3 className="text-lg font-medium text-gray-900">Billing information</h3>
+
+                                <div className="mt-6 flex items-center">
+                                    <input
+                                        id="same-as-shipping"
+                                        name="same-as-shipping"
+                                        type="checkbox"
+                                        defaultChecked
+                                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                                    />
+                                    <div className="ml-2">
+                                        <label htmlFor="same-as-shipping" className="text-sm font-medium text-gray-900">
+                                            Same as shipping information
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="mt-10 flex justify-end border-t border-gray-200 pt-6">
+                                <button
+                                    type="submit"
+                                    className="rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-black shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
+                                >
+                                    Pay now
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </section>
             </div>
         </div>
     );
 };
 
-export default Transaction;
+export default Checkout;
 
 // Fetch data from Supabase database
 export async function getServerSideProps({ params }) {
@@ -55,7 +265,7 @@ export async function getServerSideProps({ params }) {
 
     return {
         props: {
-            transaction: data,
+            checkout: data,
         },
     };
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
   ],
   theme: {
     extend: {},
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+      require('@tailwindcss/forms')
+    ],
   }
 }
