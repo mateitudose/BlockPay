2708eb0eac96e02ee805c444a4e953ae6da53058
RazvanDRL <razdir97@gmail.com>
2023-05-26 22:53:07 +0300

    Update [id].js

--- frontend/pages/invoice/[id].js
+++ frontend/pages/invoice/[id].js
@@ -21,10 +21,8 @@ import Badge from '@/components/Badge';
 import UTC from '@/components/UTC';
 
 const navigation = [
-    { name: 'Home', href: '#' },
-    { name: 'Invoices', href: '#' },
-    { name: 'Clients', href: '#' },
-    { name: 'Expenses', href: '#' },
+    { name: 'Home', href: '/' },
+
 ]
 
 function classNames(...classes) {
@@ -131,18 +129,6 @@ const Invoice = ({ invoice }) => {
                         ))}
                     </nav>
                     <div className="flex flex-1 items-center justify-end gap-x-8">
-                        <button type="button" className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500">
-                            <span className="sr-only">View notifications</span>
-                            <BellIcon className="h-6 w-6" aria-hidden="true" />
-                        </button>
-                        <a href="#" className="-m-1.5 p-1.5">
-                            <span className="sr-only">Your profile</span>
-                            <img
-                                className="h-8 w-8 rounded-full bg-gray-800"
-                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                alt=""
-                            />
-                        </a>
                     </div>
                 </div>
                 <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
@@ -208,66 +194,6 @@ const Invoice = ({ invoice }) => {
                                     </div>
                                     <div className="mt-1 text-base font-semibold leading-6 text-gray-900">{invoice.product_name}</div>
                                 </h1>
-                            </div>
-                            <div className="flex items-center gap-x-4 sm:gap-x-6">
-                                <button type="button" className="hidden text-sm font-semibold leading-6 text-gray-900 sm:block">
-                                    Copy URL
-                                </button>
-                                <a href="#" className="hidden text-sm font-semibold leading-6 text-gray-900 sm:block">
-                                    Edit
-                                </a>
-                                <a
-                                    href="#"
-                                    className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                >
-                                    Send
-                                </a>
-
-                                <Menu as="div" className="relative sm:hidden">
-                                    <Menu.Button className="-m-3 block p-3">
-                                        <span className="sr-only">More</span>
-                                        <EllipsisVerticalIcon className="h-5 w-5 text-gray-500" aria-hidden="true" />
-                                    </Menu.Button>
-
-                                    <Transition
-                                        as={Fragment}
-                                        enter="transition ease-out duration-100"
-                                        enterFrom="transform opacity-0 scale-95"
-                                        enterTo="transform opacity-100 scale-100"
-                                        leave="transition ease-in duration-75"
-                                        leaveFrom="transform opacity-100 scale-100"
-                                        leaveTo="transform opacity-0 scale-95"
-                                    >
-                                        <Menu.Items className="absolute right-0 z-10 mt-0.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <button
-                                                        type="button"
-                                                        className={classNames(
-                                                            active ? 'bg-gray-50' : '',
-                                                            'block w-full px-3 py-1 text-left text-sm leading-6 text-gray-900'
-                                                        )}
-                                                    >
-                                                        Copy URL
-                                                    </button>
-                                                )}
-                                            </Menu.Item>
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        href="#"
-                                                        className={classNames(
-                                                            active ? 'bg-gray-50' : '',
-                                                            'block px-3 py-1 text-sm leading-6 text-gray-900'
-                                                        )}
-                                                    >
-                                                        Edit
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                        </Menu.Items>
-                                    </Transition>
-                                </Menu>
                             </div>
                         </div>
                     </div>
